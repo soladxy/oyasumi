3bb23afb275fdd1a2dacf2b9016c5c3ef5d6d90d
淺い空 <971181317@qq.com>
2025-01-26 18:33:03 +0800

    add go proxy

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@ RUN_NAME=hertz_service
 mkdir -p output/bin
 cp script/* output 2>/dev/null
 chmod +x output/bootstrap.sh
+# 设置开启go mod
+go env -w GO111MODULE=auto
+# 设置go代理
+go env -w GOPROXY=https://goproxy.cn
 go build -o output/bin/${RUN_NAME}
